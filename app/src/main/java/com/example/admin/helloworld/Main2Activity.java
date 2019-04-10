2fac22d9f0e592cf7211cc284a64db9632e0fa48
admin <123>
2019-04-10 08:37:14 +0800

    1

--- app/src/main/java/com/example/admin/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/admin/helloworld/Main2Activity.java
@@ -2,34 +2,41 @@ package com.example.admin.helloworld;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
     private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.textView=(TextView)findViewById(R.id.abc);
-        String weatherId="CN101190401";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=2a4ad245ad794c8b9279c9192737bc56";
+        setContentView(R.layout.activity_main2);
+        this.textView = (TextView)findViewById(R.id.textView);
+        this.button=(Button)findViewById(R.id.button);
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+            }
+        });
+        String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
             @Override
             public void onFailure(Call call, IOException e) {
 
             }
             @Override
-            public void onResponse(Call call,Response response) throws IOException{
+            public void onResponse(Call call, Response response) throws IOException{
                 final String responseText = response.body().string();
                 runOnUiThread(new Runnable() {
                     @Override
