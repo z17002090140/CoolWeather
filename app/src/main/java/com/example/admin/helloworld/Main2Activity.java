def8ded92e5de2e981d2e58588b01271aa07bb6b
admin <123>
2019-04-12 09:08:28 +0800

    1

--- app/src/main/java/com/example/admin/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/admin/helloworld/Main2Activity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -14,30 +20,34 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private  String[] data={"北京","浙江","安徽"};
     private TextView textView;
     private Button button;
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         this.textView = (TextView)findViewById(R.id.textView);
+        this.listview=(ListView)findViewById(R.id.listview);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        listview.setAdapter(adapter);
+
         this.button=(Button)findViewById(R.id.button);
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+                startActivity(new Intent(Main2Activity.this,Main3Activity.class));
             }
         });
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
             @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-            @Override
             public void onResponse(Call call, Response response) throws IOException{
                 final String responseText = response.body().string();
+                String[] result=parseJSONWithJSONObject(responseText);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -45,6 +55,26 @@ public class Main2Activity extends AppCompatActivity {
                     }
                 });
             }
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
         });
+    }
+
+    private String[] parseJSONWithJSONObject(String responseText) {
+        JSONArray jsonArray= null;
+        try {
+            jsonArray = new JSONArray(responseText);
+            String[] result=new String[jsonArray.length()];
+            for(int i=0;i<jsonArray.length();i++){
+                JSONObject jsonObject=jsonArray.getJSONObject(i);
+                result[i]=jsonObject.getString("name");
+            }
+            return result;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
