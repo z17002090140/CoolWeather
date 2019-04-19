306353b592873e7f7b28e42f061d74e284277790
admin <123>
2019-04-19 08:57:03 +0800

    1

--- app/src/main/java/com/example/admin/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/admin/helloworld/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.admin.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,6 +16,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,9 +25,11 @@ import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private  String[] data={"北京","浙江","安徽"};
+//    private List<String>data2=new ArrayList();
+    private  int[] pids=new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    private  String[] data={"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     private TextView textView;
-    private Button button;
+//    private Button button;
     private ListView listview;
 
     @Override
@@ -34,21 +40,30 @@ public class Main2Activity extends AppCompatActivity {
         this.listview=(ListView)findViewById(R.id.listview);
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listview.setAdapter(adapter);
-
-        this.button=(Button)findViewById(R.id.button);
-        this.button.setOnClickListener(new View.OnClickListener() {
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Main2Activity.this,Main3Activity.class));
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("点击了哪一个",Main2Activity.this.pids[position]+":"+Main2Activity.this.data[position]);
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
+                intent.putExtra("pid",Main2Activity.this.pids[position]);
+                startActivity(intent);
             }
         });
+
+//        this.button=(Button)findViewById(R.id.button);
+//        this.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Main2Activity.this,Main3Activity.class));
+//            }
+//        });
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
             @Override
             public void onResponse(Call call, Response response) throws IOException{
                 final String responseText = response.body().string();
-                String[] result=parseJSONWithJSONObject(responseText);
-                Main2Activity.this.data=result;
+               parseJSONWithJSONObject(responseText);
+//                Main2Activity.this.data=result;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -63,19 +78,21 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    private String[] parseJSONWithJSONObject(String responseText) {
+    private void parseJSONWithJSONObject(String responseText) {
         JSONArray jsonArray= null;
         try {
             jsonArray = new JSONArray(responseText);
             String[] result=new String[jsonArray.length()];
             for(int i=0;i<jsonArray.length();i++){
-                JSONObject jsonObject=jsonArray.getJSONObject(i);
+                JSONObject jsonObject = null;
+                jsonObject = jsonArray.getJSONObject(i);
                 this.data[i]=jsonObject.getString("name");
+                this.pids[i]=jsonObject.getInt("id");
             }
-            return result;
+//            return result;
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return null;
+//        return null;
     }
 }
--- app/src/main/java/com/example/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/admin/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.helloworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -20,9 +21,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        int pid = intent.getIntExtra("pid",0);
+        Log.i("我们接受到了id",""+pid);
         this.textView=(TextView)findViewById(R.id.abc);
-        String weatherId="CN101210701";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=2a4ad245ad794c8b9279c9192737bc56";
+//        String weatherId="CN101210701";
+        String weatherUrl = "http://guolin.tech/api/china/" +pid;
         HttpUtil.sendOkHttpRequest(weatherUrl,new Callback(){
             @Override
             public void onFailure(Call call, IOException e) {
